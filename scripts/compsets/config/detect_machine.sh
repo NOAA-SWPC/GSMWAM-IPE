443fb8188742e1deb566275c7b2da24d5389b61d
Adam Kubaryk <Adam.Kubaryk@noaa.gov>
2020-04-02 09:31:44 -0600

    Fixes for WCOSS and Cheyenne systems
    Path adjustments for WCOSS Phase 3, removal of support for P1&2.
    
    Missing module added for Cheyenne so compilation can proceed.

--- scripts/compsets/config/detect_machine.sh
+++ scripts/compsets/config/detect_machine.sh
@@ -3,32 +3,16 @@
 
 if [[ -e /gpfs/hps && -e /usrx ]] ; then
 	MACHINE_ID=wcoss
-	if [[ -d /usrx && -d /global && -e /etc/redhat-release && \
-	-e /etc/prod ]] ; then
-		# We are on WCOSS Phase 1 or 2.
-		if ( ! cat /proc/cpuinfo |grep 'processor.*32' ) ; then
-			# Fewer than 32 fake (hyperthreading) cpus, so Phase 1.
-			FMID=wcoss.phase1
-			export BUILD_TARGET=wcoss_phase1
-			PEX=1
-		else
-			# phase 2
-			FMID=wcoss.phase2
-			export BUILD_TARGET=wcoss_phase2
-			PEX=2
-		fi
+	if [[ ! -e /cm ]] ; then
+		# phase 3
+		FMID=wcoss.phase3
+		export BUILD_TARGET=wcoss_dell_p3
+		PEX=3
 	else
-		if [[ ! -e /cm ]] ; then
-			# phase 3
-			FMID=wcoss.phase3
-			export BUILD_TARGET=wcoss_dell_p3
-			PEX=3
-		else
-			# WCOSS Cray
-			FMID=wcoss.cray
-			export BUILD_TARGET=wcoss_cray
-			PEX=c
-		fi
+		# WCOSS Cray
+		FMID=wcoss.cray
+		export BUILD_TARGET=wcoss_cray
+		PEX=c
 	fi
 elif [[ -e /xcatpost && -e /scratch1 && -e /scratch2 ]] ; then
 	export FMID=hera
